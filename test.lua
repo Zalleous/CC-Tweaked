be03d5535b601bbaf9f85a975d058d57a40ba731
Zalleous <132717375+Zalleous@users.noreply.github.com>
2024-07-07 21:59:47 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,4 +6,27 @@ local function displayFuelLevel ()
   
 end
 
+local function stripMine (n)
+
+    for i = n,0,-1 do
+    
+    turle.dig()
+
+    turle.forward()
+
+    turtle.turnLeft()
+
+    turtle.dig()
+
+    turtle.turnRight() 
+    turtle.turnRight()
+
+    turle.dig()
+
+    turtle.turnLeft()
+
+    end
+end
+
 displayFuelLevel()
+stripMine()
